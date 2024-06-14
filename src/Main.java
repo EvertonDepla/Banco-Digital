4ae82121b79ea56db93f9f3e8ee1a0ddc60c3abc
Everton Deplá <evertondutra257@gmail.com>
2024-06-13 22:35:31 -0300

    fisrt commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente everton = new Cliente();
+        everton.setNome("Everton");
+
+        Conta cc = new ContaCorrente(everton);
+        Conta poupança = new ContaPoupança(everton);
+        
+        cc.depositar(100);
+        cc.transferir(100, poupança);
+
+        cc.imprimirExtrato();
+        poupança.imprimirExtrato();
+    }
+}
